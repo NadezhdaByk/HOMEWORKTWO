b0a3c958515f192b94692200fbd6937460f3ab22
Nadezhda <nbykova79@gmail.com>
2022-07-01 19:17:50 +0300

    написали программу показывающую 3 цифрув числе

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// выводит третью цифру заданного числа
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+string ShowSecondCount(int num1)
+{
+
+string mass = num1.ToString();
+int [] array = new int [mass.Length];
+if (num1 < 99) 
+{
+    return "У вашего числа нет третьей цифры";
+}
+else
+
+for ( int i=0; i < mass.Length; i++)
+{
+    array [i] = int.Parse(mass[i].ToString());
+}
+return $"Третья цифра в числе {array[2]}";
+
+}
+Console.WriteLine(ShowSecondCount(num));
